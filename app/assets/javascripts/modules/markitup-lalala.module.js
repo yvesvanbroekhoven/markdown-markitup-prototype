69d1a4cbc42b0125463413457e03564fd1e58fdc
Yves Van Broekhoven <yvesvanbroekhoven@gmail.com>
2013-03-17 18:42:45 +0100

    Added markdown cheatsheet

--- app/assets/javascripts/modules/markitup-lalala.module.js
+++ app/assets/javascripts/modules/markitup-lalala.module.js
@@ -1,14 +1,41 @@
+//
+// Go fullscreen
+//
 $(document).on('click', '.markItUp .fullscreen', function(event) {
   var $markitup_wrapper = $(this).closest('.markItUp');
   $markitup_wrapper.addClass('fullscreen');
 });
 
+
+//
+// Close fullscreen
+//
 $(document).on('click', '.markItUp .close-fullscreen', function(event) {
   var $markitup_wrapper = $(this).closest('.markItUp');
 
   $markitup_wrapper.removeClass('fullscreen');
 });
 
+
+//
+// Toggle Markdown cheatsheet
+//
+$(document).on('click', '.markItUp .markdown-cheatsheet', function(event) {
+  var $markdown_cheatsheet = $('#markdown-cheatsheet');
+
+  if ( $markdown_cheatsheet.is(':visible') ) {
+    $markdown_cheatsheet.fadeOut(300);
+
+  } else {
+    $markdown_cheatsheet.fadeIn(300);
+
+  }
+});
+
+
+//
+// Mark It Up settings
+//
 var markdown_settings = {
   nameSpace:          'markdown-editor', // Useful to prevent multi-instances CSS conflict
   previewParserPath:  'markitup/preview',
@@ -21,16 +48,30 @@ var markdown_settings = {
     { name: 'Bold', key: 'B', openWith: '**', closeWith: '**', className: 'bold' },
     { name: 'Italic', key: 'I', openWith: '_', closeWith: '_', className: 'italic' },
     { separator: '---' },
-    { name: 'Link', key: 'L', openWith: '[', closeWith: ']([![Url:!:http://]!] "[![Title]!]")', placeHolder: 'Your text to link here...', className: 'add-link' },
+    { name: 'Link', key: 'L', openWith: '[[![Link text]!]', closeWith: ']([![Url:!:http://]!])', className: 'add-link' },
     { separator: '---' },
     { name: 'Preview', call: 'preview', className: "preview" },
     { name: 'Fullscreen', className: 'fullscreen' },
-    { name: 'Close fullscreen', className: 'close-fullscreen' }
+    { name: 'Close fullscreen', className: 'close-fullscreen' },
+    { separator: '---' },
+    { name: 'Markdown cheatsheet', className: 'markdown-cheatsheet' }
   ]
 };
+
 
 // Initialize
 $('textarea').markItUp(markdown_settings);
 
 // Initialize preview
 $('.markItUpHeader .preview').trigger('mouseup');
+
+// Load Markdown syntax cheatsheet
+$.get('/markdown-cheatsheet', function(data) {
+  var $div = $('<div id="markdown-cheatsheet" />');
+
+  $div
+    .html( data )
+    .hide();
+
+  $div.appendTo('body');
+});
--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -5,7 +5,7 @@ class DefaultController < ApplicationController
   end
 
   def markitup_preview
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, tables: true)
     @content = markdown.render(params[:data])
     render layout: 'markitup_preview'
   end
